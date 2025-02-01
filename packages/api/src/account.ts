458ee057536beab7ffef9f89f6c371afcea1cb2a
Vincent Llanto <vincentllanto098123@gmail.com>
2025-02-01 23:57:08 +0800

    Fix wrong endpoint

--- packages/api/src/account.ts
+++ packages/api/src/account.ts
@@ -2,7 +2,7 @@ import { BaseApi } from './base';
 import { GetAccountResult } from './types/account';
 
 export class Account extends BaseApi {
-  private static baseEndpoint = '/block';
+  private static baseEndpoint = '/account';
 
   static getAccount(address: string) {
     return super.get<unknown, GetAccountResult>(
