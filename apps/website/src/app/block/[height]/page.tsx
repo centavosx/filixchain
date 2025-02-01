1854ab9bcf75ac83b78cc6fe16cc426a1b5a1432
Vincent Llanto <vincentllanto098123@gmail.com>
2025-02-01 23:26:15 +0800

    integrate block endpoint

--- apps/website/src/app/block/[height]/page.tsx
+++ apps/website/src/app/block/[height]/page.tsx
@@ -9,16 +9,25 @@ import {
 import { Separator } from '@/components/ui/separator';
 import { Typography } from '@/components/ui/typography';
 import { Block } from '@ph-blockchain/api';
+import { Minter, Transaction } from '@ph-blockchain/block';
 import { Transform } from '@ph-blockchain/transformer';
 
 export type BlockHeightProps = {
   params: Promise<{ height: string }>;
 };
 export default async function BlockHeight({ params }: BlockHeightProps) {
-  const transactionHash = (await params).height;
-  const transaction = (await Block.getTransactionById(transactionHash)).data;
+  const height = (await params).height;
+  const block = (await Block.getBlockByHeight(height)).data;
 
-  const isMint = !('fixedFee' in transaction);
+  const transactions =
+    block.transactions?.map((encoded) => {
+      const mintOrTx =
+        encoded.length === Minter.ENCODED_SIZE
+          ? Minter.decode(encoded)
+          : Transaction.decode(encoded);
+
+      return mintOrTx.serialize();
+    }) ?? [];
 
   return (
     <div className="flex flex-col p-6 gap-8">
@@ -26,27 +35,26 @@ export default async function BlockHeight({ params }: BlockHeightProps) {
         <Card>
           <CardHeader>
             <CardTitle>
-              <Typography>Block</Typography>
+              <Typography>Block Height: {block.height}</Typography>
             </CardTitle>
             <CardDescription>
-              <Typography>Hash: {transaction.transactionId}</Typography>
-              <Typography>Previous: {transaction.transactionId}</Typography>
-              <Typography>Merkle: {transaction.transactionId}</Typography>
-              <Typography>Target: {transaction.transactionId}</Typography>
+              <Typography>Hash: {block.blockHash}</Typography>
+              <Typography>Previous: {block.previousHash}</Typography>
+              <Typography>Merkle: {block.merkleRoot}</Typography>
+              <Typography>Target: {block.targetHash}</Typography>
               <Typography>
                 Created:{' '}
-                {Transform.date.formatToReadable(Number(transaction.timestamp))}
+                {Transform.date.formatToReadable(Number(block.timestamp))}
               </Typography>
 
               <Typography as="p" className="mt-4 font-bold text-lg">
-                Transaction Size:{' '}
-                {Transform.date.formatToReadable(Number(transaction.timestamp))}
+                Transaction Size: {block.transactionSize}
               </Typography>
             </CardDescription>
           </CardHeader>
           <Separator className="mb-6" />
           <CardContent className="flex flex-row gap-8">
-            <TransactionTable data={[]} />
+            <TransactionTable data={transactions} shouldExcludeBlock />
           </CardContent>
         </Card>
       </section>
